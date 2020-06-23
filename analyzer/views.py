81cbf398ea80b271577cd9933f8582e2adbc62e5
= <zainr7989@gmail.com>
2020-06-23 08:30:25 -0400

    Add AnalysisCreate view controller.

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from .forms import EmptyForm
+from .forms import EmptyForm, AnalysisForm
 from django.views.generic import TemplateView
 from django.http import JsonResponse
 from .tasks import start_watching
+from django.views.generic.edit import CreateView
+from .models import Analysis
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -21,3 +23,15 @@ class HomeView(TemplateView):
                 (request.POST["file_url"],), time_limit=60 * 20, soft_time_limit=60 * 15
             )
             return JsonResponse({"task_id": res.id})
+
+
+class AnalysissCreate(CreateView):
+    '''Submit a form to create new Analysis.'''
+    model = Analysis
+    form_class = AnalysisForm
+    template_name = "pages/home.html"
+
+    def form_valid(self, form):
+        '''Initializes image field (if there is one) of new Analysis.'''
+        form.instance.image = self.request.FILES.get('image')
+        return super().form_valid(form)
