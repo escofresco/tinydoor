098b8688a2623770a68fd883db67031659608737
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-23 13:26:02 -0700

    Fix import mistake

--- watch/tasks.py
+++ watch/tasks.py
@@ -3,12 +3,11 @@ import celery
 from config.celery_app import app
 from celery import shared_task
 from .helpers import *
-from .tasks import VideoDetect
+from .video_detect import VideoDetect
 
 
 @shared_task
 def start_watching(file_url):
-    vd = VideoDetect(file_url)
     return {
         "people_count": 33,
     }
