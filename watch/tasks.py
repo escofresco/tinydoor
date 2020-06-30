70edc72e0d07cc6e08f58cad1c050bc0bdbc34ad
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-30 09:55:24 -0700

    Use video detect

--- watch/tasks.py
+++ watch/tasks.py
@@ -1,13 +1,19 @@
-from __future__ import absolute_import, unicode_literals
-import celery
-from config.celery_app import app
 from celery import shared_task
-from .helpers import *
-from .video_detect import VideoDetect
 
+from config.celery_app import app
+from doorknob import VideoDetect
 
 @shared_task
 def start_watching(file_url):
-    return {
-        "people_count": 33,
-    }
+    analyzer = VideoDetect(file_url)
+    results = None
+    analyzer.StartLabelDetection()
+
+    if analyzer.GetSQSMessageSuccess() == True:
+        results = analyzer.GetLabelDetectionResults()
+    analyzer.DeleteTopicandQueue()
+    return results
+    # return {
+    #
+    #     "people_count": 33,
+    # }
