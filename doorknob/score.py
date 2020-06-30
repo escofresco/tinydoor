e89b678f1f6d7bed89b1b7e19da64207e550ed60
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-30 09:54:58 -0700

    New lib

--- doorknob/score.py
+++ doorknob/score.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 
+__all__ = ("score", )
+
 def score(ratings, weights, scale=100, M=100, P=.5):
     ## adapted from https://tinyurl.com/y88kvfth
     assert len(ratings) == len(weights)
@@ -17,7 +19,7 @@ def score(ratings, weights, scale=100, M=100, P=.5):
     return (Decimal(P) * Decimal(p) +
             Decimal(10) * (1 - Decimal(P)) * (1 - Decimal(-q / Q).exp()))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ratings = (1,1,-1,-1,1,1,1,1,-1,1,-1)
     weights = (.5,.2,.2,.1345,.666666,.93,.5,0,.999999999999999,.7645,.789)
     print(score(ratings, weights))
