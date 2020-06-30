39e514af4cf34d5a020a285e0231fb97a18fd2cc
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-29 22:35:26 -0700

    Add first working draft of scoring algorithm

---
+++ watch/score.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+
+def score(ratings, weights, scale=100, M=100, P=.5):
+    ## adapted from https://tinyurl.com/y88kvfth
+    assert len(ratings) == len(weights)
+
+    # Number of ratings or weights
+    q = Decimal(len(ratings))
+
+    # Quantity importance
+    Q = -Decimal(M) / Decimal(1/2).ln()
+
+    # Weighted mean
+    p = (sum(Decimal(r)*Decimal(w)
+             for r,w in zip(ratings, weights)) / q)
+
+    return (Decimal(P) * Decimal(p) +
+            Decimal(10) * (1 - Decimal(P)) * (1 - Decimal(-q / Q).exp()))
+
+if __name__ == '__main__':
+    ratings = (1,1,-1,-1,1,1,1,1,-1,1,-1)
+    weights = (.5,.2,.2,.1345,.666666,.93,.5,0,.999999999999999,.7645,.789)
+    print(score(ratings, weights))
+
+    ratings = (-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1)
+    weights = (.5,.2,.2,.1345,.666666,.93,.5,0,.999999999999999,.7645,.789)
+    print(score(ratings, weights))
+
+    ratings = (-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1)
+    weights = (1,1,1,1,1,1,1,1,1,1,1)
+    print(score(ratings, weights))
