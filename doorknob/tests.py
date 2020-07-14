07d5e83de045dcd68debbb2ec2cb8bec1e66f4ee
= <zainr7989@gmail.com>
2020-07-14 16:50:08 -0400

    Init tests for the doorknob package.

---
+++ doorknob/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from .foyer import Scene
+from .detect import VideoDetect
+import helpers
+
+class VideoDetectTests(unittest.TestCase):
+    """
+    Tests that the VideoDetect properties and functions work correctly.
+    """
+
+
+
+class SceneTests(unittest.TestCase):
+    """
+    Tests that the Scene properties and functions work correctly.
+    """
+
+if __name__ == "__main__":
+    unittest.main()
