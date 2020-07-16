f33d603372dc8478dba310c6d3c9ae23cc21bea1
= <zainr7989@gmail.com>
2020-07-16 09:59:46 -0400

    Comment out Score model instantiation, fix container on front-end.

--- watch/migrations/0001_initial.py
+++ watch/migrations/0001_initial.py
@@ -15,12 +15,40 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Score',
+            name="Score",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('video_link', models.CharField(blank=True, help_text='Link to the video being scored (can play it back later).', max_length=200, null=True)),
-                ('emotion_score', models.FloatField(help_text='Tinydoor score for gauging positive customer experience.')),
-                ('user', models.ForeignKey(help_text='User who posted the video being scored.', on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "video_link",
+                    models.CharField(
+                        blank=True,
+                        help_text="Link to the video being scored (can play it back later).",
+                        max_length=200,
+                        null=True,
+                    ),
+                ),
+                (
+                    "emotion_score",
+                    models.FloatField(
+                        help_text="Tinydoor score for gauging positive customer experience."
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        help_text="User who posted the video being scored.",
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
