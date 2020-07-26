16909ee568c2e64080b952b7c5b28ebedfa3f56e
= <zainr7989@gmail.com>
2020-07-26 11:07:31 -0400

    Print emails to the console in dev.

--- config/settings/base.py
+++ config/settings/base.py
@@ -215,7 +215,7 @@ X_FRAME_OPTIONS = "DENY"
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = env(
-    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
+    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
 EMAIL_TIMEOUT = 5
